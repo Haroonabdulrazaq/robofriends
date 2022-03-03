6aa9d720f932c8c06efe68cbd448fa53cdda1b30
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-03-03 14:16:18 +0000

    refactor folder structure

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
   return (
-    <div style={{overflowY:'scroll', border:'5px solid black', height: '70vh', width: '80%', margin:'0 auto' }}>
+    <div style={{overflowY:'scroll', border:'5px solid black', height: '80vh', width: '80%', margin:'0 auto' }}>
       {props.children}
     </div>
   )
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import 'tachyons';
 
 
